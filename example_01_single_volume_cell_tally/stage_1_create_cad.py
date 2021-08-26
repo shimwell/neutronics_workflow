50dfbe12f0db022e902a24e449c51cf5b22db8bd
Jonathan <mail@jshimwell.com>
2021-08-26 20:42:36 +0100

    added initial example

---
+++ example_01_single_volume_cell_tally/stage_1_create_cad.py
@@ -0,0 +1,18 @@
+
+# This minimal example makes a 3D volume and exports the shape to a stp file
+# A surrounding volume called a graveyard is needed for neutronics simulations
+
+import paramak
+
+my_shape = paramak.ExtrudeStraightShape(
+    points=[
+        (400, 100),
+        (400, 200),
+        (600, 200),
+        (600, 100)
+        ],
+    distance = 180,
+)
+
+my_shape.export_stp('steel.stp')
+my_shape.export_graveyard('graveyard.stp')
---
+++ example_01_single_volume_cell_tally/stage_2_convert_cad_to_neutronics.py
@@ -0,0 +1,16 @@
+
+# This script converts the CAD stp files generated into h5m files that can be
+# used in DAGMC enabled codes. h5m files created in this way are imprinted,
+# merged, faceted and ready for use in OpenMC. One of the key aspects of this
+# is the asssignment of materials to the volumes present in the CAD files.
+
+from cad_to_h5m import cad_to_h5m
+
+cad_to_h5m(
+    files_with_tags=[
+        {'filename':'steel.stp', 'material_tag':'mat1'},
+        {'filename':'graveyard.stp', 'material_tag':'graveyard'},
+    ],
+    h5m_filename='dagmc.h5m',
+    cubit_path='/opt/Coreform-Cubit-2021.5/bin/'
+)
