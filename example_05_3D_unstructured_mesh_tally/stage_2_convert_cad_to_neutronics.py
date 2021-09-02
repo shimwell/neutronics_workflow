f8b59bd779f6cc46d6f826235691a6b50c8405d7
Jonathan <mail@jshimwell.com>
2021-09-02 23:07:31 +0100

    started umesh example

---
+++ example_05_3D_unstructured_mesh_tally/stage_2_convert_cad_to_neutronics.py
@@ -0,0 +1,26 @@
+
+# This script converts the CAD stp files generated into h5m files that can be
+# used in DAGMC enabled codes. h5m files created in this way are imprinted,
+# merged, faceted and ready for use in OpenMC. One of the key aspects of this
+# is the asssignment of materials to the volumes present in the CAD files.
+
+from cad_to_h5m import cad_to_h5m
+
+cad_to_h5m(
+    files_with_tags=[
+        {
+            'cad_filename':'stage_1_output/steel.stp',
+            'material_tag':'mat1',
+            'tet_mesh': 'size 10',
+        },
+        {
+            'cad_filename':'stage_1_output/graveyard.stp',
+            'material_tag':'graveyard'
+        },
+    ],
+    make_watertight=False,
+    h5m_filename='stage_2_output/dagmc.h5m',
+    cubit_path='/opt/Coreform-Cubit-2021.5/bin/',
+    h5_filename='stage_2_output/unstructured_mesh.h5'
+)
+
